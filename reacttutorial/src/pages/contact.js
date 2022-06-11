111faf3e4341847a0828330b02bc262e81be59f0
Ankit <ankit4333@gmail.com>
2022-06-11 16:21:14 +0530

    Changes from feature branch
    Signed-off-by: Ankit <ankit4333@gmail.com>

--- reacttutorial/src/pages/contact.js
+++ reacttutorial/src/pages/contact.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Contact() {
-  return <div>This is contact page changes</div>;
+  return <div>This is contact page changes is done from feature branch</div>;
 }
 
 export default Contact;
