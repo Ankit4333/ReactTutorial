36dee8c07fa6bdbc5d4dcb42386ac3b657cacdfa
Ankit <ankit4333@gmail.com>
2022-06-11 16:27:05 +0530

    Resolved the review comments

--- reacttutorial/src/pages/contact.js
+++ reacttutorial/src/pages/contact.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Contact() {
-  return <div>This is contact page changes is done from feature branch</div>;
+  return (
+    <div>
+      This is contact page changes is done from feature branch latest changes
+    </div>
+  );
 }
 
 export default Contact;
